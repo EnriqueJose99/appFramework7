936d04ec4383fb3ac59422ff0e6d9296be34bf76
EnriqueJose99 <enrique.galeano99@yahoo.com>
2020-06-11 19:49:17 -0600

    Add files via upload

---
+++ js/my-app.js
@@ -0,0 +1,34 @@
+// Initialize your app
+var app = new Framework7({
+	id: 'com.app',
+  	root: '#app',
+	theme: 'ios',
+	view: {
+        pushState: false
+    },
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'My App',
+	dialog: {
+		title: 'Diseño web 2 ',
+		buttonOk: 'Aceptar',
+  	},
+	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+		},
+		{
+		path: '/about/',
+    	url: 'about.html',
+    	name: 'about',
+  		}
+	]
+});
+// Export selectors engine
+var $$ = Dom7;
+
+$$('#btnAlerta').on('click',function() {
+	app.dialog.alert('Hola Diseño web 2');
+});
